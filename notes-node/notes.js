209027bfe038a2e429519651abbd7e4d913b481f
Kyle Desktop <kylespresser@gmail.com>
2018-11-30 11:43:27 -0500

    lecture 16

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,24 @@
 console.log('starting notes.js');
 
+const fs = require('fs');
+
 const addNote = (title, body) => {
-  console.log('Adding note', title, body);
+  let notes = [];
+  const note = {
+    title,
+    body
+  };
+
+  try {
+    let notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (e) {}
+
+  let duplicateNotes = notes.filter(note => note.title === title);
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
 };
 
 const getAll = () => {
