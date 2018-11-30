eeb59d4556fc7518d365bfe97fd88fbbf47b9e6d
Kyle Desktop <kylespresser@gmail.com>
2018-11-30 09:32:10 -0500

    lecture 10

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,7 +1,16 @@
-console.log('starting app');
+console.log('starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes');
 
-let user = os.userInfo();
-fs.appendFileSync('greetings.txt', `hello ${user.username}`);
+// let user = os.userInfo();
+// fs.appendFileSync(
+//   'greetings.txt',
+//   `hello ${user.username}. You are ${notes.age}.`
+// );
+
+let res = notes.addNote();
+console.log(res);
+
+console.log(notes.add(9, -2));
