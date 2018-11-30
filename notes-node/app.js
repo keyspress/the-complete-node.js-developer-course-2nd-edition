9cf607a1dd661f057114fdec46ccc4664dc6f250
Kyle Desktop <kylespresser@gmail.com>
2018-11-30 09:53:51 -0500

    lecture 11

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,7 +2,13 @@ console.log('starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes');
+
+// console.log(_.isString(true));
+// console.log(_.isString('string'));
+let filteredArray = _.uniq(['kyle', 'kyle', 1, 2, 1, 'kyle']);
+console.log(filteredArray);
 
 // let user = os.userInfo();
 // fs.appendFileSync(
@@ -10,7 +16,7 @@ const notes = require('./notes');
 //   `hello ${user.username}. You are ${notes.age}.`
 // );
 
-let res = notes.addNote();
-console.log(res);
+// let res = notes.addNote();
+// console.log(res);
 
-console.log(notes.add(9, -2));
+// console.log(notes.add(9, -2));
