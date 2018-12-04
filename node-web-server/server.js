a6e4ee14179b87bb9a3eb81832b9a75db6b8c3d7
Kyle Desktop <kylespresser@gmail.com>
2018-12-04 13:21:31 -0500

    lecture 43

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,19 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello from Express</h1>');
-  res.send({
-    name: 'Kyle',
-    likes: ['Food', 'Drinking']
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Well hello there.... You come here often?'
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {
