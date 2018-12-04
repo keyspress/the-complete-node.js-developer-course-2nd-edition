d004042d425db50ca5ccd838b692f218611e23b3
Kyle Desktop <kylespresser@gmail.com>
2018-12-04 10:59:16 -0500

    lecture 35

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,39 +1,39 @@
-const request = require('request');
 const yargs = require('yargs');
 
-const keys = require('./keys');
 const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: 'address',
-//       describe: 'Address to fetch weather for',
-//       string: true
-//     }
-//   })
-//   .help()
-//   .alias('help', 'h').argv;
-
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   if (errorMessage) {
-//     console.log(errorMessage);
-//   } else {
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-// });
-
-request(
-  {
-    url: `https://api.darksky.net/forecast/${keys.darkSkyKey}/-21.4666,39.6833`,
-    json: true
-  },
-  (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      console.log(body.currently.temperature);
-    } else {
-      console.log('Unable to fetch weather.');
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
     }
+  })
+  .help()
+  .alias('help', 'h').argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(results.address);
+    weather.getWeather(
+      results.latitude,
+      results.longitude,
+      (errorMessage, weatherResults) => {
+        if (errorMessage) {
+          console.log(errorMessage);
+        } else {
+          console.log(
+            `It's currently ${weatherResults.temperature}. It feels like ${
+              weatherResults.apparentTemperature
+            }.`
+          );
+        }
+      }
+    );
   }
-);
+});
---
+++ weather-app/weather/weather.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+const keys = require('../keys');
+
+const getWeather = (lat, lng, callback) => {
+  request(
+    {
+      url: `https://api.darksky.net/forecast/${keys.darkSkyKey}/${lat},${lng}`,
+      json: true
+    },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        callback(undefined, {
+          temperature: body.currently.temperature,
+          apparentTemperature: body.currently.apparentTemperature
+        });
+      } else {
+        callback('Unable to fetch weather.');
+      }
+    }
+  );
+};
+
+module.exports.getWeather = getWeather;
