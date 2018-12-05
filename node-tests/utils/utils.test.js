c4f49678f2c8843b3447e2765b0a40e4fe3bb355
Kyle Desktop <kylespresser@gmail.com>
2018-12-05 10:05:34 -0500

    lecture 53

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,38 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   const res = utils.add(33, 11);
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+
+  expect(res)
+    .toBe(44)
+    .toBeA('number');
 });
 
 it('should square a number', () => {
   const res = utils.square(2);
-  if (res !== 4) {
-    throw new Error(`Expected 4, but got ${res}`);
-  }
+
+  expect(res)
+    .toBe(4)
+    .toBeA('number');
 });
+
+it('should verify first and last names are set', () => {
+  const user = {
+    location: 'Someplace'
+  };
+  utils.setName(user, 'Kyle Spresser');
+  expect(user)
+    .toBeA('object')
+    .toInclude({ firstName: 'Kyle', lastName: 'Spresser' });
+});
+
+// it('should expect some values', () => {
+//   // expect(12).toNotBe(11);
+//   // expect({ name: 'Andrew' }).toEqual({ name: 'Andrew' });
+//   // expect([2, 3, 4]).toExclude(8);
+//   expect({ name: 'Kyle', age: 42, location: 'Lakeland' }).toInclude({
+//     age: 42
+//   });
+// });
