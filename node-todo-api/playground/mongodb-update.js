33bf33dd2d4c505d4130e95c8c9f0c089fdaef16
Kyle Desktop <kylespresser@gmail.com>
2018-12-05 14:25:30 -0500

    lecture 68

---
+++ node-todo-api/playground/mongodb-update.js
@@ -0,0 +1,53 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect(
+  'mongodb://localhost:27017/TodoApp',
+  (err, db) => {
+    if (err) {
+      return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos')
+    //   .findOneAndUpdate(
+    //     {
+    //       _id: new ObjectID('5c081f1bbf2e062cb090f49b')
+    //     },
+    //     {
+    //       $set: {
+    //         completed: true
+    //       }
+    //     },
+    //     {
+    //       returnOriginal: false
+    //     }
+    //   )
+    //   .then(result => {
+    //     console.log(result);
+    //   });
+
+    db.collection('Users')
+      .findOneAndUpdate(
+        {
+          _id: new ObjectID('5c081548dcef932fc9b2eae7')
+        },
+        {
+          $set: {
+            name: 'Kyle'
+          },
+          $inc: {
+            age: 1
+          }
+        },
+        {
+          returnOriginal: false
+        }
+      )
+      .then(result => {
+        console.log(result);
+      });
+
+    // db.close();
+  }
+);
