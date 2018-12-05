1bfbff548f45d4a8a7a549096e77e8257317706e
Kyle Desktop <kylespresser@gmail.com>
2018-12-05 15:06:41 -0500

    lecture 71

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -5,30 +5,55 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 const Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minLength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
+
+const User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minLength: 1,
+    trim: true
+  }
+});
+
+const newUser = new User({
+  email: 'kylespresser@gmail.com'
+});
+
+newUser.save().then(
+  doc => {
+    console.log('Saved user', doc);
+  },
+  e => {
+    console.log('Unable to add user', e);
+  }
+);
 
 // const newTodo = new Todo({
 //   text: 'Cook dinner'
 // });
-const newTodo = new Todo({
-  text: 'Walk the fish',
-  completed: true,
-  completedAt: 1
-});
+// const newTodo = new Todo({
+//   text: 'Edit a video  '
+// });
 
-newTodo.save().then(
-  doc => {
-    console.log('Saved todo', doc);
-  },
-  e => {
-    console.log('Unable to save todo');
-  }
-);
+// newTodo.save().then(
+//   doc => {
+//     console.log('Saved todo', doc);
+//   },
+//   e => {
+//     console.log('Unable to save todo');
+//   }
+// );
