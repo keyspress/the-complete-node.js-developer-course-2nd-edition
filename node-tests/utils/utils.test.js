b81ea6dff5da024f222274f3b59c51f896351420
Kyle Desktop <kylespresser@gmail.com>
2018-12-05 10:19:10 -0500

    lecture 54

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -10,12 +10,30 @@ it('should add two numbers', () => {
     .toBeA('number');
 });
 
+it('should async add two numbers', done => {
+  utils.asyncAdd(4, 3, sum => {
+    expect(sum)
+      .toBe(7)
+      .toBeA('number');
+    done();
+  });
+});
+
 it('should square a number', () => {
   const res = utils.square(2);
 
   expect(res)
     .toBe(4)
     .toBeA('number');
+});
+
+it('should async square a number', done => {
+  utils.asyncSquare(2, num => {
+    expect(num)
+      .toBe(4)
+      .toBeA('number');
+    done();
+  });
 });
 
 it('should verify first and last names are set', () => {
