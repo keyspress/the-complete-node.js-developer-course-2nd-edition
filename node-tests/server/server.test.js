69280312bb75638816fc8d8a3023e963d97210ec
Kyle Desktop <kylespresser@gmail.com>
2018-12-05 11:19:59 -0500

    lecture 57

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -3,24 +3,29 @@ const expect = require('expect');
 
 const app = require('./server').app;
 
-it('should return hello world response', done => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect(res => {
-      expect(res.body).toInclude({
-        error: 'Page not found.'
-      });
-    })
-    .end(done);
-});
-
-it('should find name in array', done => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect(res => {
-      expect(res.body).toInclude({ name: 'Kyle', age: 42 });
-    })
-    .end(done);
+describe('Server', () => {
+  describe('GET /', () => {
+    it('should return hello world response', done => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect(res => {
+          expect(res.body).toInclude({
+            error: 'Page not found.'
+          });
+        })
+        .end(done);
+    });
+  });
+  describe('GET /users', () => {
+    it('should find name in array', done => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect(res => {
+          expect(res.body).toInclude({ name: 'Kyle', age: 42 });
+        })
+        .end(done);
+    });
+  });
 });
