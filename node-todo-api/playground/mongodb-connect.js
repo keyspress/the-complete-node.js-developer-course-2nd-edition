0df37d5eef49823c3f4ce33959b707e671b9be00
Kyle Desktop <kylespresser@gmail.com>
2018-12-05 13:24:47 -0500

    lecture 64

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 MongoClient.connect(
   'mongodb://localhost:27017/TodoApp',
@@ -21,19 +22,21 @@ MongoClient.connect(
     //   }
     // );
 
-    db.collection('Users').insertOne(
-      {
-        name: 'Kyle',
-        age: 42,
-        location: 'Lakeland'
-      },
-      (err, result) => {
-        if (err) {
-          return console.log('Unable to insert user', err);
-        }
-        console.log(JSON.stringify(result.ops, undefined, 2));
-      }
-    );
+    // db.collection('Users').insertOne(
+    //   {
+    //     name: 'Kyle',
+    //     age: 42,
+    //     location: 'Lakeland'
+    //   },
+    //   (err, result) => {
+    //     if (err) {
+    //       return console.log('Unable to insert user', err);
+    //     }
+    //     console.log(
+    //       JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2)
+    //     );
+    //   }
+    // );
 
     db.close();
   }
