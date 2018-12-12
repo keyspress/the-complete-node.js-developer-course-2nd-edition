fe061b77de9a573a1ffebe95a0b8f1a70e1daaa2
Kyle Desktop <kylespresser@gmail.com>
2018-12-12 12:12:09 -0500

    lecture 97

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -55,6 +55,16 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+UserSchema.methods.removeToken = function(token) {
+  const user = this;
+
+  return user.update({
+    $pull: {
+      tokens: { token }
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function(token) {
   var User = this;
   // const User = this;
