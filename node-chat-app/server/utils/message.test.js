2eb1f3f2e3ab3ac3fbd3ad378a4b945fdeeeef4b
Kyle Desktop <kylespresser@gmail.com>
2018-12-13 12:02:36 -0500

    lecture 111

---
+++ node-chat-app/server/utils/message.test.js
@@ -0,0 +1,14 @@
+const expect = require('expect');
+
+const { generateMessage } = require('./message');
+
+describe('generateMessage', () => {
+  it('should generate the correct message object', () => {
+    const from = 'test';
+    const text = 'test text';
+    const message = generateMessage(from, text);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({ from, text });
+  });
+});
