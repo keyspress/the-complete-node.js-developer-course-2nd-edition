b53054663f83beb8a109a6b58a1a674c02f32411
Kyle Desktop <kylespresser@gmail.com>
2018-12-13 10:44:33 -0500

    lecture 109

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Jim',
+    text: 'Yo',
+    createdAt: 111
+  });
+
+  socket.on('createMessage', message => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => [console.log('page disconnected')]);
 });
 
