900ae3a5c091f7fd956ee46e7d529ecec20012f7
Kyle Desktop <kylespresser@gmail.com>
2018-12-14 10:23:40 -0500

    lecture 118

--- node-chat-app/playground/time.js
+++ node-chat-app/playground/time.js
@@ -1,5 +1,10 @@
 const moment = require('moment');
 
-const date = moment();
-// date.add(100, 'year').subtract(2, 'hour');
-console.log(date.format('MMM Do, YYYY h:mm a'));
+// const date = moment();
+// // date.add(100, 'year').subtract(2, 'hour');
+// console.log(date.format('MMM Do, YYYY h:mm a'));
+
+const createdAt = 1234;
+
+const date = moment(createdAt);
+console.log(date.format('h:mm a'));
