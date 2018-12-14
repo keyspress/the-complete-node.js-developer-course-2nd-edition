671853ebb69cf83e6d81795de53084d1ae7cbba6
Kyle Desktop <kylespresser@gmail.com>
2018-12-14 14:48:42 -0500

    lecture 122

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const { generateMessage, generateLocationMessage } = require('./utils/message');
+const { isRealString } = require('./utils/validation');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
@@ -23,6 +24,14 @@ io.on('connection', socket => {
     'newMessage',
     generateMessage('Admin', 'New user joined')
   );
+
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      callback('Name and room name are required.');
+    }
+
+    callback();
+  });
 
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
