74d10747d065fd162ad94d858a2e5a1e13c30f91
Kyle Desktop <kylespresser@gmail.com>
2018-12-18 11:38:32 -0500

    lecture 127

--- node-chat-app/public/js/chat.js
+++ node-chat-app/public/js/chat.js
@@ -79,7 +79,6 @@ jQuery('#message-form').on('submit', function(e) {
   socket.emit(
     'createMessage',
     {
-      from: 'User',
       text: jQuery(messageTextbox).val()
     },
     function() {
