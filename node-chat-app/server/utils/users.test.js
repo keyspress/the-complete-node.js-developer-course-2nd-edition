1c1532125c7da0a6013c740dd1883b04872c8e28
Kyle Desktop <kylespresser@gmail.com>
2018-12-18 09:48:28 -0500

    lecture 124

---
+++ node-chat-app/server/utils/users.test.js
@@ -0,0 +1,17 @@
+const expect = require('expect');
+
+const { Users } = require('./users');
+
+describe('Users', () => {
+  it('should add new user', () => {
+    const users = new Users();
+    const user = {
+      id: '123',
+      name: 'Test',
+      room: 'Testing Room'
+    };
+    const resUser = users.addUser(user.id, user.name, user.room);
+
+    expect(users.users).toEqual([user]);
+  });
+});
